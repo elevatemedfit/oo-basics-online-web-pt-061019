55981b92b1150f4685c9e43fa0474044b4e33bc2
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-21 21:20:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,17 +2,9 @@ class Book
   def initialize(title)
     @title = title
   end
-  def initialize(author)
-    @author = author
-  end
-  def author=(author)
-    @author = author
-  end
+
   def title
     @title
   end
-  def author
-    @author
-  end
-
+  
 end
